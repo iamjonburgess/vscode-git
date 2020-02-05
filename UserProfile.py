fbf2940cf9c7370e4ea6fa1a0d92d8581d79d465
Jonathan Burgess <JonMacbook@Jonathans-MBP.fios-router.home>
2020-02-05 11:47:05 -0500

    Created UserProfile.py foundation

---
+++ UserProfile.py
@@ -0,0 +1,15 @@
+""" UserProfile.py by Jonathan Burgess, 2020-Feb-5
+This is a program to take input from the user in order
+to create a profile for a database. """
+
+# Input First Name
+input("Enter Your First Name: ")
+
+# Input Last Name
+input("Enter Your Last Name: ")
+
+# Input Email
+input("Enter Your Email: ")
+
+# Input Password
+input("Create a Password: ")
